d596b4eac59abcd1e6aa8280263c49b6e65bd550
Roberto Alcaraz <roberto@irba.es>
2021-03-21 11:25:54 +0100

    new commit

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import dash
-<<<<<<< HEAD
 from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_core_components as dcc
@@ -10,16 +9,4 @@ import json
 
 df1 = pd.read_csv('https://raw.githubusercontent.com/RoberAlcaraz/First-Take-Away/main/vehicles_data.csv')
 
-
-
-
-
-
-=======
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-
-import plotly.express as px
-import pandas as pd
->>>>>>> 339b31fe05804915c929bfbd42ee17d14d542e7b
+# hello 
---
+++ packages.R
@@ -0,0 +1,4 @@
+# install.packages("pacman")
+pacman::p_load(reticulate)
+py_install("dash")
+py_install("dash-bootstrap-components")
