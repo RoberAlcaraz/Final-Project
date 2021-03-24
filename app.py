2a7762baf899d5423ed5d9b020abe29ecb5cba73
amaliajimenezajt <amaliajimenez.ajt@gmail.com>
2021-03-24 10:59:15 +0100

    plot summary added

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ def update_graphs(rows, derived_virtual_selected_rows):
             figure={
                 "data": [
                     {
-                        "x": dff["country"],
+                        "x": dff["Education_Level"],
                         "y": dff[column],
                         "type": "bar",
                         "marker": {"color": colors},
@@ -245,7 +245,7 @@ def update_graphs(rows, derived_virtual_selected_rows):
         # check if column exists - user may have deleted it
         # If `column.deletable=False`, then you don't
         # need to do this check.
-        for column in ["pop", "lifeExp", "gdpPercap"] if column in dff
+        for column in ["Total_Revolving_Bal", "Total_Trans_Amt", "Total_Trans_Ct"] if column in dff
     ]
 
 
